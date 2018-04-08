1d38b0807a5b826ea463810bb5661d012ae8c98f
Adjoa Darien <adjoa.darien@gmail.com>
2018-04-08 19:12:32 +0000

    Done.

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  // code to be executed goes here
+  // jQuery("#text").append("this is so freaking cool.");
+  // is the same as...
+  $("#text").append("this is so freaking cool.");
+});
